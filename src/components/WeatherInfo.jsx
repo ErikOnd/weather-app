330e8b1654dd193dfed511b78268e0a7a9968640
Erik <erik.ondra@yahoo.com>
2023-02-12 16:28:25 +0100

    weather icon update

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -20,16 +20,43 @@ const WeatherInfo = ({ info, setInfo, weatherArr }) => {
 
 
 
+    const getImg = (cloudiness, maxTemp) => {
+
+        if (cloudiness < 10) {
+            return 'https://cdn-icons-png.flaticon.com/512/869/869869.png'
+        }
+
+        if (cloudiness < 60) {
+            return 'https://cdn-icons-png.flaticon.com/512/9586/9586736.png'
+        }
+
+        if (cloudiness < 90) {
+            return 'https://cdn-icons-png.flaticon.com/512/473/473697.png '
+        }
+
+        if (cloudiness > 90 && maxTemp <= 0) {
+            return 'https://cdn-icons-png.flaticon.com/512/7334/7334205.png '
+        }
+
+        if (cloudiness > 90) {
+            return 'https://cdn-icons-png.flaticon.com/512/7038/7038436.png'
+        }
+
+    }
+
+
+
+
     return (
         <>
             <Col className="mt-5">
                 <Card className="weather-card">
-                    <Card.Img variant="top" className="p-5" src="https://cdn-icons-png.flaticon.com/512/1779/1779940.png" />
+                    <Card.Img variant="top" className="p-5" src={getImg(info.clouds.all, kelvinToCelsius(info.main.temp_max))} />
                     <Card.Body className="cart-text text-left">
                         <Card.Title className="location-name"><b>{info.name}</b></Card.Title>
                         <Card.Text className="d-flex flex-column">
                             <span><b>Weather: </b>{info.weather[0].description}</span>
-                            <span><b>Temperature</b>: {kelvinToCelsius(info.main.temp_min)}-{kelvinToCelsius(info.main.temp_max)}°C</span>
+                            <span><b>Temperature</b>: {kelvinToCelsius(info.main.temp_max)}° / {kelvinToCelsius(info.main.temp_min)}°</span>
                             <span><b>Humidity: </b>{info.main.humidity}%</span>
                             <span><b>Wind: </b>{meterPerSecToKmPerHour(info.wind.speed)}km/h</span>
                         </Card.Text>
@@ -53,11 +80,8 @@ export default WeatherInfo
 
 /*
 
-
+todo: update images
 todo: weather forecast
-todo: getting location automatically
-
-
 
 
 */
